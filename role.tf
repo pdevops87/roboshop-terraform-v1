d3978d71d684d8f7e16bb7d1300e96a3bbcea657
Your Name <vemasani.pavani@gmail.com>
2025-12-04 15:08:32 +0530

    install ansible

--- role.tf
+++ role.tf
@@ -1,16 +1,16 @@
 # create a role and attach trust policy directly
-resource "aws_iam_role" "role" {
-  name               = "test-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
+# resource "aws_iam_role" "role" {
+#   name               = "test-role-r"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
