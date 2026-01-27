76594f52f1805b8781f4bd494e94da98fe99941b
Your Name <vemasani.pavani@gmail.com>
2026-01-27 12:05:52 +0530

    move to /app directory

--- main.tf
+++ main.tf
@@ -28,13 +28,14 @@ resource "null_resource" "provisioner" {
 
   }
   for_each      = var.components
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = aws_instance.instance[each.key].private_ip
-  }
+
   provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.instance[each.key].private_ip
+    }
     inline = [
       "sudo dnf install python3.11-pip -y",
       "sudo pip3.11 install ansible",
